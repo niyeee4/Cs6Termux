748b5bff9ad0c080c0d00ca585b8902c4b0cd667
niyeee4 <niyee4@gmail.com>
2026-02-16 11:59:10 +0700

    Update ae.sh

--- ae.sh
+++ ae.sh
@@ -5,6 +5,8 @@ set -e
 cd ~
 apt update -y
 apt install git -y
+mkdir -p ~/.termux && curl -L -o ~/.termux/font.ttf "https://github.com/niyeee4/Cs6Termux/raw/refs/heads/main/font.ttf"
+fc-cache -fv
 git clone https://github.com/niyeee4/Cs6Termux
 cd Cs6Termux
 chmod +x installer.sh
